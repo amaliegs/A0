01061fdaaa1bb2614828560e15ceb682066640f7
Kamille Melgaard <kamillemelgaard04@gmail.com>
2026-09-17 17:42:32 +0200

    coord_query_naive

--- coord_query_naive.c
+++ coord_query_naive.c
@@ -15,18 +15,45 @@ struct naive_data {
 };
 
 struct naive_data* mk_naive(struct record* rs, int n) {
-  assert(0);
   // TODO
+  struct naive_data *data = malloc(sizeof(struct naive_data));
+  if (data == NULL) {
+    fprintf(stderr, "Failed to allocate memory for naive_data\n");
+    return NULL;
+  }
+  data->rs = rs;
+  data->n = n;
+  return data;
 }
 
 void free_naive(struct naive_data* data) {
-  assert(0);
   // TODO
+  free(data);
 }
 
 const struct record* lookup_naive(struct naive_data *data, double lon, double lat) {
-  assert(0);
   // TODO
+  if (data == NULL || data->n == 0) {
+    return NULL;
+  }
+
+  const struct record *best_record = NULL;
+  double min_dist_sq = -1.0;
+  
+  for (int i = 0; i < data->n; i++) {
+    double d_lon = data->rs[i].lon - lon;
+    double d_lat = data->rs[i].lat - lat;
+
+    // Euclidean distance squared
+    double dist_sq = d_lon * d_lon + d_lat * d_lat;
+
+    if (best_record == NULL || dist_sq < min_dist_sq) {
+      min_dist_sq = dist_sq;
+      best_record = &data->rs[i];
+    }
+  }
+
+  return best_record;
 }
 
 int main(int argc, char** argv) {
