12e0900f5e4e95edb02500d24331af53d2504652
Amalie Sørensen <clp606@alumni.ku.dk>
2026-09-21 13:17:17 +0200

    Kdtree

--- coord_query_kdtree.c
+++ coord_query_kdtree.c
@@ -5,7 +5,6 @@
 #include <stdint.h>
 #include <errno.h>
 #include <assert.h>
-
 #include "record.h"
 #include "coord_query.h"
 
@@ -13,12 +12,14 @@ struct naive_data {
   struct record *rs;
   int n;
 };
+struct Node* build_kdtree(Point points[], int n, int depth) {
+    if (n <= 0) return NULL;
 
-struct naive_data* mk_naive(struct record* rs, int n) {
+    int axis = depth % k;
   // TODO
-  struct naive_data *data = malloc(sizeof(struct naive_data));
-  if (data == NULL) {
-    fprintf(stderr, "Failed to allocate memory for naive_data\n");
+  struct kdtree_data *data = malloc(sizeof(struct kdtree_data));
+  if (n <= 0) return NULL {
+    fprintf(stderr, "Failed to allocate memory for kdtree_data\n");
     return NULL;
   }
   data->rs = rs;
@@ -26,39 +27,31 @@ struct naive_data* mk_naive(struct record* rs, int n) {
   return data;
 }
 
-void free_naive(struct naive_data* data) {
+node create_node(points, depth) {
+    axis <- depth mod d
+    select median by axis from points
+    node <- new node
+    node.point <- median
+    node.axis <- axis
+    node.left <- create_node (points before median, depth+1)
+    node.right <- create_node (points after median, depth+1)
+    return node
+}
+
+void free_kdtree(struct kdtree_data* data) {
   // TODO
   free(data);
 }
-
-const struct record* lookup_naive(struct naive_data *data, double lon, double lat) {
-  // TODO
-  if (data == NULL || data->n == 0) {
-    return NULL;
-  }
-
-  const struct record *best_record = NULL;
-  double min_dist_sq = -1.0;
+const struct record* lookup_kdtree(struct kdtree_data *data, double lon, double lat) {
+    if (Node == NULL) {
+        return NULL;
+    } else if { 
+        (Node.points >
   
-  for (int i = 0; i < data->n; i++) {
-    double d_lon = data->rs[i].lon - lon;
-    double d_lat = data->rs[i].lat - lat;
-
-    // Euclidean distance squared
-    double dist_sq = d_lon * d_lon + d_lat * d_lat;
-
-    if (best_record == NULL || dist_sq < min_dist_sq) {
-      min_dist_sq = dist_sq;
-      best_record = &data->rs[i];
-    }
-  }
-
-  return best_record;
 }
-
 int main(int argc, char** argv) {
   return coord_query_loop(argc, argv,
-                          (mk_index_fn)mk_naive,
-                          (free_index_fn)free_naive,
-                          (lookup_fn)lookup_naive);
+                          (mk_index_fn)mk_kdtree,
+                          (free_index_fn)free_kdtree,
+                          (lookup_fn)lookup_kdtree);
 }
