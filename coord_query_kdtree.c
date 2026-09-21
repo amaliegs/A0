1ef8e8372d74227023c1deaec1d476240e07aa17
Kristoffer <khrolschau@gmail.com>
2026-09-21 14:55:30 +0200

    kdtree

--- coord_query_kdtree.c
+++ coord_query_kdtree.c
@@ -8,50 +8,42 @@
 #include "record.h"
 #include "coord_query.h"
 
-struct naive_data {
-  struct record *rs;
-  int n;
-};
-struct Node* build_kdtree(Point points[], int n, int depth) {
-    if (n <= 0) return NULL;
 
-    int axis = depth % k;
-  // TODO
-  struct kdtree_data *data = malloc(sizeof(struct kdtree_data));
-  if (n <= 0) return NULL {
-    fprintf(stderr, "Failed to allocate memory for kdtree_data\n");
-    return NULL;
+static struct kdtree(struct record **points, int n, int depth) {
+    if (n <= 0) {
+      return NULL;
+    }
+
+    // 2 dimensions, 0 = longitude and 1 = latitude.
+    int axis = depth % 2;
+    int mid = n / 2;
+
+    // Select median by axis from points
+    select(points, 0, n, mid, axis);
+
+    struct kdtree_node *node = malloc(sizeof(struct kdtree_node));
+    if (node == NULL) {
+      fprintf(stderr, "Failed to allocate memory for naive_data\n")
+    }
+
+    node->point = points[median];
+    node->axis = axis;
+    
+    // Points before median
+    node->left = kdtree(points, mid, depth + 1);
+
+    // Points after median
+    node->right = kdtree(points + mid + 1, n - mid - 1, depth + 1);
+
+    return node;
+}
+
+static void free_node(struct kdtree_node *node) {
+  if (node == NULL) {
+    return;
   }
-  data->rs = rs;
-  data->n = n;
-  return data;
-}
-
-node create_node(points, depth) {
-    axis <- depth mod d
-    select median by axis from points
-    node <- new node
-    node.point <- median
-    node.axis <- axis
-    node.left <- create_node (points before median, depth+1)
-    node.right <- create_node (points after median, depth+1)
-    return node
-}
-
-void free_kdtree(struct kdtree_data* data) {
-  // TODO
-  free(data);
-}
-const struct record* lookup_kdtree(struct kdtree_data *data, double lon, double lat) {
-    if (Node == NULL) {
-        return NULL;
-    } else if { 
-        (Node.points >
   
-}
-int main(int argc, char** argv) {
-  return coord_query_loop(argc, argv,
-                          (mk_index_fn)mk_kdtree,
-                          (free_index_fn)free_kdtree,
-                          (lookup_fn)lookup_kdtree);
+  free_node(node->left);
+  free_node(node->right);
+  free(node);
 }
